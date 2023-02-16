f4d4fde6fa427803091e38a4615d160053e8836a
macbook <mishatret2010@gmail.com>
2023-02-16 21:13:33 +0200

    v1

---
+++ main.py
@@ -0,0 +1,15 @@
+# class Great_Grandma:
+#     height = 160
+#     age = 90
+#
+# class Dad(Great_Grandma):
+#     age = 40
+#
+# class Kid(Dad):
+#     height = 50
+#
+#     def __init__(self):
+#         print(f'Height - {self.height}')
+#         print(f'Age - {self.age}')
+#
+# nick = Kid()
