9c2e8a73a0e481f547e56fc23b1e8e4c2231b0ba
macbook <mishatret2010@gmail.com>
2023-02-17 19:23:07 +0200

    v2 final

--- main.py
+++ main.py
@@ -1,3 +1,31 @@
+class food:
+    food = "food"
+    _food = "_food"
+    __food = "__food"
+
+    def __init__(self):
+        self.water = "water"
+        self._water = "_water"
+        self.__water = "__water"
+
+    def printer(self):
+        print(self.food)
+        print(self._food)
+        print(self.__food)
+        print(self.water)
+        print(self._water)
+        print(self.__water)
+
+class printout(food):
+    def hi_printer(self):
+        print(self.food)
+        print(self._food)
+        #print(self.__food)
+        print(self.water)
+        print(self._water)
+        #print(self.__water)
+
+
 # class Great_Grandma:
 #     height = 160
 #     age = 90
